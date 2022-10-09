a5f272ed5866e5c382e931ec5e9047c3a43d26c7
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2022-10-09 10:25:59 +0200

    Added mathematical methods to be tested

---
+++ 1/Calculator/MathematicalMethods.cs
@@ -0,0 +1,23 @@
+﻿namespace Calculator
+{
+    internal class MathematicalMethods
+    {
+        public static double Add(double firstNumber, double secondNumber) => firstNumber + secondNumber;
+        public static double Subtract(double firstNumber, double secondNumber) => firstNumber - secondNumber;
+        public static double Multiply(double firstNumber, double secondNumber) => firstNumber * secondNumber;
+        public static double Divide(double firstNumber, double secondNumber) => firstNumber / secondNumber;
+        public static int Reminder(int firstNumber, int secondNumber) => firstNumber % secondNumber;
+        public static int Factorial(int number)
+        {
+            if(number == 0)
+                return 1;
+
+            int result = 1;
+
+            for(int i = 1; i<=number; i++)
+                result *= i;
+            
+            return result;
+        }
+    }
+}
