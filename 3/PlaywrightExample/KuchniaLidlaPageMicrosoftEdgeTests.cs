f6d7dc8b5c57fd16b1365fc51adee646cdd3113c
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2022-11-18 15:37:20 +0100

    Added same tests for microsoft edge browser

---
+++ 3/PlaywrightExample/KuchniaLidlaPageMicrosoftEdgeTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using PlaywrightExample.PageObjects;
+
+namespace PlaywrightExample
+{
+    public class KuchniaLidlaPageMicrosoftEdgeTests
+    {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        
+        [SetUp]
+        public async Task SetupGoogleChromeBrowser()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Channel = "msedge",
+                Headless   = false,
+                SlowMo = 2000
+            });
+        }
+
+        [TearDown]
+        public async Task DisposeBrowser() => await _browser.DisposeAsync();
+
+        [Test]
+        public async Task FilteringRecipes_IsSuggesting_CorrectRecipes()
+        {
+            var page = await _browser.NewPageAsync();
+            var kuchniaLidlaPage = new KuchniaLidlaPage(page);
+            
+            await page.GotoAsync("https://kuchnialidla.pl/");
+
+            await kuchniaLidlaPage.ClickAcceptCookies();
+            await kuchniaLidlaPage.SearchForRecipe("pierogi z kaczka");
+            await kuchniaLidlaPage.ClickDuckDumplingsRecipe();
+            await kuchniaLidlaPage.RecipeHeaderIsVisible();
+        }
+    }
+}
