14858445d928810a20759fc9f805e3cebefcbf1e
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2022-12-02 14:26:39 +0100

    Added calculator class for tests

---
+++ 4/MockStubTAU/Calculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(double firstNumber, double secondNumber) => firstNumber + secondNumber;
+
+        public double Subtract(double firstNumber, double secondNumber) => firstNumber - secondNumber;
+
+        public double Multiply(double firstNumber, double secondNumber) => firstNumber * secondNumber;
+
+        public int Reminder(int firstNumber, int secondNumber) => firstNumber % secondNumber;
+
+        public double Divide(double firstNumber, double secondNumber)
+        {
+            if (secondNumber == 0)
+                throw new Exception("Cannot divide by zero");
+
+            return firstNumber / secondNumber;
+        }
+
+        public int Factorial(int number)
+        {
+            if(number == 0)
+                return 1;
+
+            if(number < 0)
+                throw new Exception("Cannot count factorial from negative numbers");
+
+            int result = 1;
+
+            for(int i = 1; i<=number; i++)
+                result *= i;
+            
+            return result;
+        }
+    }
+}
